cfbbc427fcebd596ae64dd0c1e08a818bf034581
Titian Steiger <titian.steiger@gmail.com>
2017-07-25 18:33:19 +0200

    Add files via upload

--- get.py
+++ get.py
@@ -1,1 +1,48 @@
+# get.py
+import json
+import requests
 
+
+def get_coinlist():
+	"""
+	Return general information about all the coins available on 
+	cryptocompare.com. The function returns a dictionary with its keys being 
+	the coin symbols and the values being dictionaries containing information
+	about the specified coin.
+
+	The following parameters will be returned for each coin:
+
+	'Algorithm'            - 
+	'CoinName'
+	'FullName'
+	'FullyPremined'
+	'Id'
+	'ImageUrl'
+	'Name'
+	'PreMinedValue'
+	'ProofType'
+	'SortOrder'
+	'TotalCoinsFreeFloat'
+	'TotalCoinSupply'
+	'Url'
+	"""
+	
+	# http request
+	url = "https://www.cryptocompare.com/api/data/coinlist/"
+	r = requests.get(url)
+
+	# data extraction
+	data = r.json()
+	response = data["Response"]
+	message = data["Message"]
+	coin_data = data["Data"]
+
+	print(message)
+
+	return coin_data
+
+if __name__ == "__main__":
+	coin_data = get_coinlist()
+
+	print(len(coin_data.keys()))
+	print(coin_data["BTC"])
