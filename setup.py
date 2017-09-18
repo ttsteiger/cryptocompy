b555eb2c669c09dc0b748fe3a2f206811afb4505
titian <titian.steiger@gmail.com>
2017-09-18 22:55:08 +0200

    Corrected setup.py.

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(name='cryptocompy',
       packages=['cryptocompy'],
-      version='0.1.dev1',
+      version='0.1.1.dev1',
       description='Simple wrapper for the public Cryptocompare API.',
       keywords = '',
       author='Titian Steiger',
       author_email='titian.steiger@gmail.com',
       url='https://github.com/ttsteiger/cryptocompare-python',
-      download_url='https://github.com/ttsteiger/cryptocompare-python/archive/0.1.dev1.tar.gz',
+      download_url='https://github.com/ttsteiger/cryptocompare-python/archive/0.1.1.dev1.tar.gz',
       license='MIT',
       python_requires='>=3',
-      install_requires=['datetime', 'json', 'requests', 'time'],)
+      install_requires=[],)
