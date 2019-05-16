2274af9943d1f45b3632ab1a55eadc2bac32ef87
Romain PESCHE <romain.pesche@sewan.fr>
2019-05-16 17:42:29 +0200

    lint

--- cryptocompy/helper_functions.py
+++ cryptocompy/helper_functions.py
@@ -7,113 +7,111 @@ import time
 
 
 def build_url(func, **kwargs):
-	"""
-	"""
+    """
+    """
 
-	if func in ['coinlist', 'coinsnapshot', 'miningcontracts', 
-				'miningequipment']:
-		url = "https://www.cryptocompare.com/api/data/{}?".format(func)
-	elif func in ['exchanges', 'volumes', 'pairs']:
-		url = "https://min-api.cryptocompare.com/data/top/{}?".format(func)
-	elif func in ['minute', 'hour', 'day']:
-		url = "https://min-api.cryptocompare.com/data/histo{}?".format(func)
-	else:
-		url = "https://min-api.cryptocompare.com/data/{}?".format(func)
-	
-	# collect url parts in list
-	url_parts = []
+    if func in ['coinlist', 'coinsnapshot', 'miningcontracts',
+                'miningequipment']:
+        url = "https://www.cryptocompare.com/api/data/{}?".format(func)
+    elif func in ['exchanges', 'volumes', 'pairs']:
+        url = "https://min-api.cryptocompare.com/data/top/{}?".format(func)
+    elif func in ['minute', 'hour', 'day']:
+        url = "https://min-api.cryptocompare.com/data/histo{}?".format(func)
+    else:
+        url = "https://min-api.cryptocompare.com/data/{}?".format(func)
 
-	for key, value in kwargs.items():
-		if key == 'fsym':
-			url_parts.append("fsym={}".format(value))
-		elif key == 'fsyms':
-			url_parts.append("fsyms={}".format(",".join(value)))
-		elif key == 'tsym':
-			url_parts.append("tsym={}".format(value))
-		elif key == 'tsyms':
-			url_parts.append("tsyms={}".format(",".join(value)))
-		
-		# exchange
-		elif key == 'e' and value != 'all':
-			url_parts.append("e={}".format(value))
-		elif key == 'e' and value == 'all':
-			url_parts.append("e={}".format('CCCAGG'))
-		
-		# conversion
-		elif key == 'try_conversion' and not value:
-			url_parts.append("tryConversion=false")
-		
-		# markets
-		elif key == 'markets' and value != 'all':
-			url_parts.append("e={}".format(",".join(value)))
-		elif key == 'markets' and value == 'all':
-			url_parts.append("e={}".format('CCCAGG'))
+    # collect url parts in list
+    url_parts = []
 
-		elif key == 'avg_type' and value != 'HourVWAP':
-			url_parts.append("avgType={}".format(value))
-		elif key == 'utc_hour_diff' and value != 0:
-			url_parts.append("UTCHourDiff={}".format(value))
-		elif key == 'to_ts' and value:
-			url_parts.append("toTs={}".format(value))
-		elif key == 'aggregate' and value != 1:
-			url_parts.append("aggregate={}".format(value))
-		
-		elif key == 'limit':
-			url_parts.append("limit={}".format(value))
+    for key, value in kwargs.items():
+        if key == 'fsym':
+            url_parts.append("fsym={}".format(value))
+        elif key == 'fsyms':
+            url_parts.append("fsyms={}".format(",".join(value)))
+        elif key == 'tsym':
+            url_parts.append("tsym={}".format(value))
+        elif key == 'tsyms':
+            url_parts.append("tsyms={}".format(",".join(value)))
 
-	# put url together
-	url = url + "&".join(url_parts)
+        # exchange
+        elif key == 'e' and value != 'all':
+            url_parts.append("e={}".format(value))
+        elif key == 'e' and value == 'all':
+            url_parts.append("e={}".format('CCCAGG'))
 
-	return url
+        # conversion
+        elif key == 'try_conversion' and not value:
+            url_parts.append("tryConversion=false")
+
+        # markets
+        elif key == 'markets' and value != 'all':
+            url_parts.append("e={}".format(",".join(value)))
+        elif key == 'markets' and value == 'all':
+            url_parts.append("e={}".format('CCCAGG'))
+
+        elif key == 'avg_type' and value != 'HourVWAP':
+            url_parts.append("avgType={}".format(value))
+        elif key == 'utc_hour_diff' and value != 0:
+            url_parts.append("UTCHourDiff={}".format(value))
+        elif key == 'to_ts' and value:
+            url_parts.append("toTs={}".format(value))
+        elif key == 'aggregate' and value != 1:
+            url_parts.append("aggregate={}".format(value))
+
+        elif key == 'limit':
+            url_parts.append("limit={}".format(value))
+
+    # put url together
+    url = url + "&".join(url_parts)
+
+    return url
 
 
 def load_data(url):
-	"""
-	"""
+    """
+    """
 
-	# http request
-	r = requests.get(url)
+    # http request
+    r = requests.get(url)
 
-	# decode to json
-	data = r.json()
+    # decode to json
+    data = r.json()
 
-	return data
+    return data
 
 
 def timestamp_to_date(ts):
-		"""Convert timestamp to nice date and time format.
+    """Convert timestamp to nice date and time format.
 
-		Args:
-			ts: Timestamp as string.
+    Args:
+        ts: Timestamp as string.
 
-		Returns:
-			
-		"""
-		return datetime.datetime.fromtimestamp(int(ts)
-		                                       ).strftime('%Y-%m-%d %H:%M:%S')
+    Returns:
+
+    """
+    return datetime.datetime.fromtimestamp(int(ts)
+                                           ).strftime('%Y-%m-%d %H:%M:%S')
 
 
 def date_to_timestamp(date):
-	"""
-	
-
-	Args:
-		date: Date as string with these formats: "Y-m-d", "Y-m-d H-M-S".
-	Returns:
-	
-	"""
-	
-	# format input string if only date but no time is provided
-	if len(date) == 10:
-		date = "{} 00:00:00".format(date)
-
-	return time.mktime(time.strptime(date, '%Y-%m-%d %H:%M:%S')) # 
+    """
 
 
+    Args:
+        date: Date as string with these formats: "Y-m-d", "Y-m-d H-M-S".
+    Returns:
+
+    """
+
+    # format input string if only date but no time is provided
+    if len(date) == 10:
+        date = "{} 00:00:00".format(date)
+
+    return time.mktime(time.strptime(date, '%Y-%m-%d %H:%M:%S'))  #
 
 
 if __name__ == "__main__":
-	date = timestamp_to_date('1492689600')
-	ts = date_to_timestamp(date)
+    date = timestamp_to_date('1492689600')
+    ts = date_to_timestamp(date)
 
-	print(date, ts)
+    print(date, ts)
--- cryptocompy/price.py
+++ cryptocompy/price.py
@@ -1,291 +1,298 @@
 # price.py
 
-from .helper_functions import build_url, load_data, timestamp_to_date, \
-date_to_timestamp
+from .helper_functions import (build_url, load_data, timestamp_to_date,
+                               date_to_timestamp)
 
 
-def get_current_price(fsyms, tsyms, e='all', try_conversion=True, full=False, 
-                     format='raw'):
-	"""Get latest trading price or full trading information in display or raw 
-	format for the specified FROM/TO currency pairs.
+def get_current_price(fsyms, tsyms, e='all', try_conversion=True, full=False,
+                      format='raw'):
+    """Get latest trading price or full trading information in display or raw
+    format for the specified FROM/TO currency pairs.
 
-	Args:
-		fsyms: Single string or list of FROM symbols.
-		tsyms: Single string or list of TO symbols.
-		e: Default returns average price across all exchanges. Can be set to the
-			name of a single exchange.
-		try_conversion: If the crypto does not trade directly into the toSymbol 
-			requested, BTC will be used for conversion. If set to false, it will 
-			try to get values without using any conversion at all.
-		full: Default of False returns only the latest price. True returns the 
-			following dictionary structure containing the full trading info:
-		format: Default returns the 'RAW' format. Can be set to 'DISPLAY' 
-			format.
-	Returns:
-		Returns a dictionary containing the latest price pairs if full is set to
-		false:
-		
-		{fsym1: {tsym1: ..., tsym2:..., ...},
-		 fsym2: {...},
-		 ...}
+    Args:
+        fsyms: Single string or list of FROM symbols.
+        tsyms: Single string or list of TO symbols.
+        e: Default returns average price across all exchanges.
+            Can be set to the name of a single exchange.
+        try_conversion: If the crypto does not trade directly into the toSymbol
+            requested, BTC will be used for conversion.
+            If set to false, it will try to get values
+            without using any conversion at all.
+        full: Default of False returns only the latest price. True returns the
+            following dictionary structure containing the full trading info:
+        format: Default returns the 'RAW' format. Can be set to 'DISPLAY'
+            format.
+    Returns:
+        Returns a dictionary containing the latest price pairs
+        if full is set to false:
 
-		or full trading info dictionaries for all the price pairs in the other
-		case:
+        {fsym1: {tsym1: ..., tsym2:..., ...},
+         fsym2: {...},
+         ...}
 
-		{fsym1: {tsym1: {'CHANGE24HOUR': ...,
-		 				 'CHANGEPCT24HOUR': ...,
- 	                     'FLAGS': ...,
-				 	     'FROMSYMBOL': ...,
-					 	 'HIGH24HOUR': ...,
-					 	 'LASTMARKET': ...,
-					 	 'LASTTRADEID': ...,
-					 	 'LASTUPDATE': ..., 
-					 	 'LASTVOLUME': ...,
-					 	 'LASTVOLUMETO': ...,
-					 	 'LOW24HOUR': ...,
-					 	 'MARKET' ...,
-						 'MKTCAP': ...,
-						 'OPEN24HOUR': ...,
-						 'PRICE': ...,
-					 	 'SUPPLY': ...,
-					 	 'TOSYMBOL': ...,
-					 	 'TYPE': ..., 
-					 	 'VOLUME24HOUR': ...,
-					 	 'VOLUME24HOURTO': ...},
-				 tsym2: ..., ...},
-		fsym2: {...},
-		...}
-	"""
+        or full trading info dictionaries for all the price pairs in the other
+        case:
 
-	# select API function based on 'full' parameter value
-	if not full:
-		func = 'pricemulti'
-	else:
-		func = 'pricemultifull'
+        {fsym1: {tsym1: {'CHANGE24HOUR': ...,
+                          'CHANGEPCT24HOUR': ...,
+                          'FLAGS': ...,
+                          'FROMSYMBOL': ...,
+                          'HIGH24HOUR': ...,
+                          'LASTMARKET': ...,
+                          'LASTTRADEID': ...,
+                          'LASTUPDATE': ...,
+                          'LASTVOLUME': ...,
+                          'LASTVOLUMETO': ...,
+                          'LOW24HOUR': ...,
+                          'MARKET' ...,
+                         'MKTCAP': ...,
+                         'OPEN24HOUR': ...,
+                         'PRICE': ...,
+                          'SUPPLY': ...,
+                          'TOSYMBOL': ...,
+                          'TYPE': ...,
+                          'VOLUME24HOUR': ...,
+                          'VOLUME24HOURTO': ...},
+                 tsym2: ..., ...},
+        fsym2: {...},
+        ...}
+    """
 
-	# convert single fsym and tsym input to single element lists
-	if not isinstance(fsyms, list):
-		fsyms = [fsyms]
-	if not isinstance(tsyms, list):
-		tsyms = [tsyms]
+    # select API function based on 'full' parameter value
+    if not full:
+        func = 'pricemulti'
+    else:
+        func = 'pricemultifull'
 
-	# load data
-	url = build_url(func, fsyms=fsyms, tsyms=tsyms, e=e, 
-	                try_conversion=try_conversion)
-	data = load_data(url)
+    # convert single fsym and tsym input to single element lists
+    if not isinstance(fsyms, list):
+        fsyms = [fsyms]
+    if not isinstance(tsyms, list):
+        tsyms = [tsyms]
 
-	#  select right format to return for full requests
-	if full and format == 'raw':
-		data = data['RAW']
-	elif full and format == 'display':
-		data = data['DISPLAY']
+    # load data
+    url = build_url(func, fsyms=fsyms, tsyms=tsyms, e=e,
+                    try_conversion=try_conversion)
+    data = load_data(url)
 
-	return data
+    #  select right format to return for full requests
+    if full and format == 'raw':
+        data = data['RAW']
+    elif full and format == 'display':
+        data = data['DISPLAY']
 
-
-def get_current_trading_info(fsym, tsym, markets='all', try_conversion=True, 
-                       		 format='raw'):
-	"""
-	Get the latest trading info of the requested pair as a volume weighted 
-	average based on the markets requested.
-	
-	Args:
-		fsym: FROM symbol.
-		tsym: TO symbol.
-		markets: List containing the market names.
-		try_conversion: If the crypto does not trade directly into the toSymbol 
-			requested, BTC will be used for conversion. If set to false, it will 
-			try to get values without using any conversion at all.
-		format: Default returns the 'RAW' format. Can be set to 'DISPLAY' 
-			format.
-
-	Returns:
-		The returned latest average trading information dictionary contains
-		the following key value pairs:
-		
-		{'PRICE': ...,
-		 'LASTVOLUMETO': ...,
-		 'TOSYMBOL': ...,
-		 'LOW24HOUR': ...,
-		 'CHANGE24HOUR': ...,
-		 'FROMSYMBOL': ...,
-		 'FLAGS': ...,
-		 'VOLUME24HOUR': ...,
-		 'HIGH24HOUR': ...,
-		 'LASTUPDATE': ...,
-		 'VOLUME24HOURT': ...,
-		 'LASTMARKET': ...,
-		 'CHANGEPCT24HOUR': ...,
-		 'OPEN24HOUR': ...,
-		 'MARKET': ...,
-		 'LASTTRADEID': ...,
-		 'LASTVOLUME': ...}
-	"""
-	
-	# load data 
-	url = build_url('generateAvg', fsym=fsym, tsym=tsym, markets=markets,
-	                try_conversion=try_conversion)
-	data = load_data(url)
-
-	# select format to return
-	if format == 'raw':
-		data = data['RAW']
-	elif format == 'display':
-		data = data['DISPLAY']
-
-	return {fsym: {tsym: data}}
+    return data
 
 
-def get_day_average_price(fsym, tsym, e='all', try_conversion=True, 
-                    	  avg_type='HourVWAP', utc_hour_diff=0):
-	"""
-	Get the current days average price of a currency pair.
+def get_current_trading_info(fsym, tsym, markets='all', try_conversion=True,
+                             format='raw'):
+    """
+    Get the latest trading info of the requested pair as a volume weighted
+    average based on the markets requested.
 
-	Args:
-		fsym: FROM symbol.
-		tsym: TO symbol.
-		e: Default returns average price across all exchanges. Can be set to the
-			name of a single exchange.
-		try_conversion: If the crypto does not trade directly into the toSymbol 
-			requested, BTC will be used for conversion. If set to false, it will 
-			try to get values without using any conversion at all.
-		avg_type: 'HourVWAP' returns a volume weighted average of the hourly
-			close price. The other option 'MidHighLow' gives the average between
-			the 24 hour high and low.
-		utc_hour_diff: Pass hour difference to UTC for different time zone.
+    Args:
+        fsym: FROM symbol.
+        tsym: TO symbol.
+        markets: List containing the market names.
+        try_conversion: If the crypto does not trade directly into the toSymbol
+            requested, BTC will be used for conversion.
+            If set to false, it will try to get values
+            without using any conversion at all.
+        format: Default returns the 'RAW' format. Can be set to 'DISPLAY'
+            format.
 
-		# add 'toTs' parameter
-		######################
-		######################
-		######################
-		######################
-		######################
-		######################
-	
-	Returns:
-		Returns a price dictionairy containing the current days average price as 
-		float.
+    Returns:
+        The returned latest average trading information dictionary contains
+        the following key value pairs:
 
-		{fsym: {tsym: price}}
-	"""
+        {'PRICE': ...,
+         'LASTVOLUMETO': ...,
+         'TOSYMBOL': ...,
+         'LOW24HOUR': ...,
+         'CHANGE24HOUR': ...,
+         'FROMSYMBOL': ...,
+         'FLAGS': ...,
+         'VOLUME24HOUR': ...,
+         'HIGH24HOUR': ...,
+         'LASTUPDATE': ...,
+         'VOLUME24HOURT': ...,
+         'LASTMARKET': ...,
+         'CHANGEPCT24HOUR': ...,
+         'OPEN24HOUR': ...,
+         'MARKET': ...,
+         'LASTTRADEID': ...,
+         'LASTVOLUME': ...}
+    """
 
-	# load data
-	url = build_url('dayAvg', fsym=fsym, tsym=tsym, e=e, 
-	                try_conversion=try_conversion, avg_type=avg_type, 
-	                utc_hour_diff=utc_hour_diff)
-	data = load_data(url)
+    # load data
+    url = build_url('generateAvg', fsym=fsym, tsym=tsym, markets=markets,
+                    try_conversion=try_conversion)
+    data = load_data(url)
 
-	# remove 'ConversionType' information
-	del data['ConversionType']
-	
-	return {fsym: data}
+    # select format to return
+    if format == 'raw':
+        data = data['RAW']
+    elif format == 'display':
+        data = data['DISPLAY']
+
+    return {fsym: {tsym: data}}
+
+
+def get_day_average_price(fsym, tsym, e='all', try_conversion=True,
+                          avg_type='HourVWAP', utc_hour_diff=0):
+    """
+    Get the current days average price of a currency pair.
+
+    Args:
+        fsym: FROM symbol.
+        tsym: TO symbol.
+        e: Default returns average price across all exchanges.
+            Can be set to the name of a single exchange.
+        try_conversion: If the crypto does not trade directly into the toSymbol
+            requested, BTC will be used for conversion.
+            If set to false, it will try to get values
+            without using any conversion at all.
+        avg_type: 'HourVWAP' returns a volume weighted average of the hourly
+            close price.
+            The other option 'MidHighLow' gives the average
+            between the 24 hour high and low.
+        utc_hour_diff: Pass hour difference to UTC for different time zone.
+
+        # add 'toTs' parameter
+        ######################
+        ######################
+        ######################
+        ######################
+        ######################
+        ######################
+
+    Returns:
+        Returns a price dictionairy containing the current
+        days average price as float.
+
+        {fsym: {tsym: price}}
+    """
+
+    # load data
+    url = build_url('dayAvg', fsym=fsym, tsym=tsym, e=e,
+                    try_conversion=try_conversion, avg_type=avg_type,
+                    utc_hour_diff=utc_hour_diff)
+    data = load_data(url)
+
+    # remove 'ConversionType' information
+    del data['ConversionType']
+
+    return {fsym: data}
 
 
 def get_historical_eod_price(fsym, tsyms, date, e='all', try_conversion=True):
-	"""Get end of day price for cryptocurrency in any other currency for the 
-	requested timestamp.
+    """Get end of day price for cryptocurrency in any other currency for the
+    requested timestamp.
 
-	Args:
-		fsym: FROM symbol.
-		tsyms: Single string or list of TO symbols.
-		date: Date as string with this format: "Y-m-d H:M:S".
-		e: Default returns average price across all exchanges. Can be set to the
-			name of a single exchange.
-		try_conversion: If the crypto does not trade directly into the toSymbol 
-			requested, BTC will be used for conversion. If set to false, it will 
-			try to get values without using any conversion at all.
+    Args:
+        fsym: FROM symbol.
+        tsyms: Single string or list of TO symbols.
+        date: Date as string with this format: "Y-m-d H:M:S".
+        e: Default returns average price across all exchanges.
+           Can be set to the name of a single exchange.
+        try_conversion: If the crypto does not trade directly into the toSymbol
+            requested, BTC will be used for conversion.
+            If set to false, it will try to get values
+            without using any conversion at all.
 
-	Returns:
-		Returns a dictionary containing the end of day price pairs for the 
-		provided date.
-		
-		{fsym: {tsym1: ..., tsym2: ..., ...}}
-	"""
+    Returns:
+        Returns a dictionary containing the end of day price pairs for the
+        provided date.
 
-	# convert single fsym and tsym input to single element lists
-	if not isinstance(tsyms, list):
-		tsyms = [tsyms]
+        {fsym: {tsym1: ..., tsym2: ..., ...}}
+    """
 
-	# convert date to timestamp
-	ts = date_to_timestamp(date)
+    # convert single fsym and tsym input to single element lists
+    if not isinstance(tsyms, list):
+        tsyms = [tsyms]
 
-	# load data
-	url = build_url("pricehistorical", fsym=fsym, tsyms=tsyms, ts=ts, 
-	                e=e, try_conversion=try_conversion)
-	data = load_data(url)
+    # convert date to timestamp
+    ts = date_to_timestamp(date)
 
-	return data
+    # load data
+    url = build_url("pricehistorical", fsym=fsym, tsyms=tsyms, ts=ts,
+                    e=e, try_conversion=try_conversion)
+    data = load_data(url)
+
+    return data
 
 
-def get_historical_data(fsym, tsym, freq, info='full', e='all', 
-                        try_conversion=True, aggregate=1, limit=1440, 
+def get_historical_data(fsym, tsym, freq, info='full', e='all',
+                        try_conversion=True, aggregate=1, limit=1440,
                         to_ts=False):
-	"""Get minute-by-minute historical price and volume information for 
-	the requested currency pair. Available data is limited to the last 7 days.
-	
-	Args:
-		fsym: FROM symbol.
-		tsym: TO symbol.
-		freq: Frequency of the data. Can be set to 'minute', 'hour' or 'day'.
-		info: Select price or volume information to return. Default of 'full'
-			returns all of them. Can be set to 'high', 'low', 'open', 'close', 
-			'volumefrom', and 'volumeto' or a list containing several of those
-			values.
-		e: Default returns average price across all exchanges. Can be set to the
-		   name of a single exchange.
-		try_conversion: If the crypto does not trade directly into the toSymbol 
-			requested, BTC will be used for conversion. If set to false, it will 
-			try to get values without using any conversion at all.
-		aggregate: Aggregates the minute prices into bins of the specified size.
-		limit: Number of minute prices. The limit settings depend on the freq
-			selected:
-				minute: default = 1440, min = 1, max = 2000
-				hour: default = 168, min = 1, max 2000
-				day: default = 30, min = 1, max 2000
-			
-			Using aggregate reduces the maximum number of points that can be 
-			returned by a factor equal to the chosen bin size.
-		
-		# add 'toTs' parameter
-		######################
-		######################
-		######################
-		######################
-		######################
-		######################
-	
-	Returns:
-		List of dictionairies containing the price and volume information for
-		each requested tick.
+    """Get minute-by-minute historical price and volume information for
+    the requested currency pair. Available data is limited to the last 7 days.
 
-		[{'time': ..., 'close': ..., 'high': ..., 'low': ..., 'open': ...,
-		  'volumefrom': ..., 'volumeto': ...},
-		  {...},
-		 ...]
-	"""
+    Args:
+        fsym: FROM symbol.
+        tsym: TO symbol.
+        freq: Frequency of the data. Can be set to 'minute', 'hour' or 'day'.
+        info: Select price or volume information to return. Default of 'full'
+            returns all of them. Can be set to 'high', 'low', 'open', 'close',
+            'volumefrom', and 'volumeto' or a list containing several of those
+            values.
+        e: Default returns average price across all exchanges.
+           Can be set to the name of a single exchange.
+        try_conversion: If the crypto does not trade directly into the toSymbol
+            requested, BTC will be used for conversion.
+            If set to false, it will try to get values
+            without using any conversion at all.
+        aggregate: Aggregates the minute prices into
+                   bins of the specified size.
+        limit: Number of minute prices. The limit settings depend on the freq
+            selected:
+                minute: default = 1440, min = 1, max = 2000
+                hour: default = 168, min = 1, max 2000
+                day: default = 30, min = 1, max 2000
 
-	# load data
-	url = build_url(freq, fsym=fsym, tsym=tsym, freq=freq, e=e, 
-	                try_conversion=try_conversion, aggregate=aggregate, 
-	                limit=limit, to_ts=to_ts)
-	data = load_data(url)
-	data = data['Data']
+            Using aggregate reduces the maximum number of points that can be
+            returned by a factor equal to the chosen bin size.
 
-	# convert timestamps to nice date format
-	for d in data:
-		d['time'] = timestamp_to_date(d['time'])
+        # add 'toTs' parameter
+        ######################
+        ######################
+        ######################
+        ######################
+        ######################
+        ######################
 
-	# convert single input info to single element list
-	if not isinstance(info, list):
-		info = [info]
+    Returns:
+        List of dictionairies containing the price and volume information for
+        each requested tick.
 
-	# select information to return
-	if info[0] == 'full':
-		return data
-	else:
-		for d in data:
-			for k, v in list(d.items()):
-				if k not in info and k != 'time':
-					del d[k]
-				
-		return data
+        [{'time': ..., 'close': ..., 'high': ..., 'low': ..., 'open': ...,
+          'volumefrom': ..., 'volumeto': ...},
+          {...},
+         ...]
+    """
+
+    # load data
+    url = build_url(freq, fsym=fsym, tsym=tsym, freq=freq, e=e,
+                    try_conversion=try_conversion, aggregate=aggregate,
+                    limit=limit, to_ts=to_ts)
+    data = load_data(url)
+    data = data['Data']
+
+    # convert timestamps to nice date format
+    for d in data:
+        d['time'] = timestamp_to_date(d['time'])
+
+    # convert single input info to single element list
+    if not isinstance(info, list):
+        info = [info]
+
+    # select information to return
+    if info[0] == 'full':
+        return data
+    else:
+        for d in data:
+            for k, v in list(d.items()):
+                if k not in info and k != 'time':
+                    del d[k]
+
+        return data
